0d79de09594b9e6fed5b91f55155e6be8909cb34
HyeonWooJo <chaduri7913@gmail.com>
2022-09-19 14:00:39 +0900

    Feat: 보유 종목 API 구현

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -3,7 +3,8 @@ from rest_framework import viewsets
 from .models import *
 from .serializers import (
     InvestMainSerializer,
-    InvestDetailSerializer
+    InvestDetailSerializer,
+    HoldingSerializer
 )
 
 class InvestMainViewSet(viewsets.ModelViewSet):
@@ -16,3 +17,9 @@ class InvestDetailViewSet(viewsets.ModelViewSet):
     """투자 메인 CRUD API"""
     queryset = Account.objects.all()
     serializer_class = InvestDetailSerializer
+
+
+class HoldingViewSet(viewsets.ModelViewSet):
+    """보유 종목 CRUD API"""
+    queryset = User.objects.all()
+    serializer_class = HoldingSerializer
