5dd371d7832565cacd454b8e04378412c9af5148
HyeonWooJo <chaduri7913@gmail.com>
2022-09-19 21:04:42 +0900

    Fix: 보유 종목 조회 API 수정

--- backend/apis/serializers.py
+++ backend/apis/serializers.py
@@ -8,45 +8,45 @@ class InvestMainSerializer(serializers.ModelSerializer):
     company = serializers.SerializerMethodField()
 
     class Meta:
-        model = Account
+        model  = Account
         fields = '__all__'
 
     def get_company(self, object):
-        company = object.user.invesetmet_set.company
+        company = object.investment_set.all()[0].company
 
         return company
 
 
 class InvestDetailSerializer(serializers.ModelSerializer):
     """투자 상세 Serializer"""
-    company = serializers.SerializerMethodField()
-    principal = serializers.SerializerMethodField()
+    company        = serializers.SerializerMethodField()
+    principal      = serializers.SerializerMethodField()
     total_proceeds = serializers.SerializerMethodField()
-    profit_rate = serializers.SerializerMethodField()
+    profit_rate    = serializers.SerializerMethodField()
 
     class Meta:
         model = Account
         fields = '__all__'
 
     def get_company(self, object):
-        company = object.user.invesetmet_set.company
+        company = object.investment_set.all()[0].company
 
         return company
 
     def get_principal(self, object):
-        principal = object.user.invesetmet_set.principal
+        principal = object.investment_set.all()[0].principal
 
         return principal
 
     def get_total_proceeds(self, object):
         total_assests = object.total_assessts
-        principal = object.user.invesetmet_set.principal
+        principal     = object.investment_set.all()[0].principal
 
         return total_assests - principal
 
     def get_profit_rate(self, object):
-        total_assests = object.total_assessts
-        principal = object.user.invesetmet_set.principal
+        total_assests  = object.total_assessts
+        principal      = object.investment_set.all()[0].principal
         total_proceeds = total_assests - principal
 
         return (total_proceeds / principal * 100)
@@ -54,25 +54,25 @@ class InvestDetailSerializer(serializers.ModelSerializer):
 
 class HoldingSerializer(serializers.ModelSerializer):
     """보유 종목 Serializer"""
-    holding_name = serializers.SerializerMethodField()
-    asset_group = serializers.SerializerMethodField()
-    isin = serializers.SerializerMethodField()
+    holding_name     = serializers.SerializerMethodField()
+    asset_group      = serializers.SerializerMethodField()
+    isin             = serializers.SerializerMethodField()
     appraisal_amount = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        exclude = '__all__'
+        exclude = ['name']
 
     def get_holding_name(self, object):
-        return object.userholding_set.holding.name
+        return object.userholding_set.all()[0].holding.name
     
     def get_asset_group(self, object):
-        return object.userholding_set.holding.asset_group
+        return object.userholding_set.all()[0].holding.asset_group
 
     def get_isin(self, object):
-        return object.userholding_set.holding.isin
+        return object.userholding_set.all()[0].holding.isin
 
     def get_appraisal_amount(self, object):
-        current_price = object.userholding_set.current_price
-        quantity = object.userholding_set.quantity
+        current_price = object.userholding_set.all()[0].current_price
+        quantity      = object.userholding_set.all()[0].quantity
         return current_price * quantity
